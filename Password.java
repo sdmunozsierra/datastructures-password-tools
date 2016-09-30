756c2acdda8bb802d2c6a549fd016e699ab1e4f4
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-09-30 01:08:02 -0600

    Update Password.java

--- Password.java
+++ Password.java
@@ -1,1 +1,19 @@
+package gui_for_final;
 
+/** This class will store a Password object */
+public class Password {
+	//Data for Password
+	public String pwd;
+	public int value;
+	
+	//Default Constructor
+	public Password(String password, int value){
+		this.pwd = password;
+		this.value = value;
+	}//end constructor
+	//Empty constructor
+	public Password(){
+		pwd = " ";
+		value = 0;
+	}//end constructor
+}//end class
